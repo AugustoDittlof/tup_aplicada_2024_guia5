53be13cac10b9982b83eb653806bbf14f9ba1f66
augustodittlof <augustodittlof@gmail.com>
2024-09-05 17:46:13 -0300

    subiendo

---
+++ FormPrincipal/Producto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormPrincipal
+{
+    abstract class Producto:IComparable
+    {
+        protected double precioBase;
+        protected double largo;
+        private int codigo;
+
+        public int Codigo
+        {
+            get { return codigo; } set{codigo = value;}
+        }
+        public Producto (double precio, double largo)
+        {
+            precioBase=precio;
+            this.largo=largo;
+        }
+        public abstract double Peso();
+        public abstract double Precio();
+        public int CompareTo(object prod)
+        {
+            Producto produc=(Producto) prod;
+            return this.Codigo.CompareTo(produc.Codigo);
+        }
+
+    }
+}
